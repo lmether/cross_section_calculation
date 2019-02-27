96b78263ee408f4882e3ba08fe4530b4eddfb28d
KPoland <kyle.poland0310@gmail.com>
2019-02-27 14:00:46 +0100

    More arguments included

--- crosssection/main.py
+++ crosssection/main.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+import crosssection.save_cross_section as scs
 
 import argparse
 
 from crosssection.cross_section import AtomFactory, CrossSectionCalcBed, CrossSectionCalcBeb, CrossSectionCalcVacNote
 
-atom_collection = {'h': AtomFactory.get_hydrogen(), 'h2': AtomFactory.get_h2(), 'he': AtomFactory.get_helium(),
-                   'ne': AtomFactory.get_nitrogen(), 'n2': AtomFactory.get_nitrogen()}
+atom_library = {'h': AtomFactory.get_hydrogen(), 'h2': AtomFactory.get_h2(), 'he': AtomFactory.get_helium(),
+                   'ne': AtomFactory.get_neon(), 'n2': AtomFactory.get_nitrogen()}
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
@@ -30,7 +31,28 @@ def parse_arguments():
     parser.add_argument(
         "-e",
         "--energy",
-        help="Energy to use.",
+        help="Single energy to calculate the ionization cross section for.",
+        type=float,
+    )
+
+    parser.add_argument(
+        "-el",
+        "--energylower",
+        help="Lower boundary of the range of energies to use.",
+        type=float,
+    )
+
+    parser.add_argument(
+        "-eu",
+        "--energyupper",
+        help="Upper boundary of the range of energies to use.",
+        type=float,
+    )
+
+    parser.add_argument(
+        "-ebin",
+        "--energybinning",
+        help="Energy binning to use for the range of energies.",
         type=float,
     )
 
@@ -47,6 +69,16 @@ def parse_arguments():
     )
 
     return parser.parse_args()
+
+def init_calculation(method, Atom):
+
+    if method == 'bed':
+        return lambda T: CrossSectionCalcBed(T, atom = Atom)
+
+    if method == 'beb':
+        return lambda T: CrossSectionCalcBeb(T, atom = Atom)
+
+    raise Exception('Calculation method has not been assigned yet. Work in progress,')
 
 
 def example():
@@ -76,20 +108,51 @@ def example():
 
 def calc_CS(energy,species):
 
-    pass
+    try:
+        Atom = atom_library[species]
+        cross = init_calculation(energy, Atom.calc_method)
+
+
+    except Exception as err:
+        print(err)
+
+    return cross.calculate()
+
 
 
 def main():
+
     args = parse_arguments()
-    if args.version:
-        print('0.1 alpha')
-    if args.energy and args.species:
-        calc_CS(args.energy, args.species)
-    if args.energy:
-        print(args.energy)
-    if args.example:
-        print('Example curve for molecular hydrogen in a region 1e7 to 1e13')
-        example()
+
+    try:
+        if args.version:
+            print('0.1 alpha')
+
+        if args.example:
+            print('Example curve for molecular hydrogen in a region 1e7 to 1e13')
+            example()
+
+        if args.energy and not args.species:
+            print('No species defined. Add one via -s or --species .')
+
+        if args.species and not args.energy:
+            print('No energy defined. Add one via -e or --energy .')
+
+        if args.energy and args.species:
+            return calc_CS(args.energy, args.species)
+
+        if (args.energylower or args.energyupper) and not (args.energyupper and args.energylower):
+            raise Exception('You are missing a boundary.')
+
+        if (args.species and args.energylower and args.energyupper and args.energybinning):
+            number_datapoints = (args.energyupper - args.energylower)//args.energybinning
+            dataset = []
+            for energy in np.linspace(args.energylower, args.energyupper, number_datapoints):
+                dataset.append([energy, calc_CS(energy, args.species)])
+            scs.save_as_mat(dataset, args.energylower, args.energyupper)
+
+    except Exception as err:
+        print(err)
 
 if __name__ == "__main__":
     main()
--- crosssection/save_cross_section.py
+++ crosssection/save_cross_section.py
@@ -1,2 +1,10 @@
 import scipy.io as sio
+import os
 
+def save_as_mat(array, energy1 = '', energy2 = ''):
+
+    cross_sec_dict = {'Ionization cross section': array}
+    path = os.getcwd()
+    sio.savemat(path + '/cross_sections' + energy1 + '_' + energy2, mdict = cross_sec_dict)
+
+    return None
